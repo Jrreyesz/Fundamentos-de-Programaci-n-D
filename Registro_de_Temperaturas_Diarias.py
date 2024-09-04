8e47f29e65f8d538e7f9aafa3fdfcc6df3cb32ec
Jrreyesz <jr.reyesz@uea.edu.ec>
2024-09-04 14:50:22 -0500

    Create Registro_de_Temperaturas_Diarias.py
    Tarea Registro de Temperaturas

---
+++ Registro_de_Temperaturas_Diarias.py
@@ -0,0 +1,148 @@
+matr_temp = [
+    [#Ciudad
+        [#Semana
+            ['Semana 1', 'Piñas', 'Lunes', 30],#Días
+            ['Semana 1', 'Piñas', 'Martes', 28],
+            ['Semana 1', 'Piñas', 'Miercoles', 26],
+            ['Semana 1', 'Piñas', 'Jueves', 29],
+            ['Semana 1', 'Piñas', 'Viernes', 30],
+            ['Semana 1', 'Piñas', 'Sabado', 25],
+            ['Semana 1', 'Piñas', 'Domingo', 30],
+        ],
+        [
+            ['Semana 2', 'Piñas', 'Lunes', 33],
+            ['Semana 2', 'Piñas', 'Martes', 30],
+            ['Semana 2', 'Piñas', 'Miercoles', 26],
+            ['Semana 2', 'Piñas', 'Jueves', 24],
+            ['Semana 2', 'Piñas', 'Viernes', 18],
+            ['Semana 2', 'Piñas', 'Sabado', 20],
+            ['Semana 2', 'Piñas', 'Domingo', 21],
+        ],
+        [
+            ['Semana 3', 'Piñas', 'Lunes', 30],
+            ['Semana 3', 'Piñas', 'Martes', 25],
+            ['Semana 3', 'Piñas', 'Miercoles', 26],
+            ['Semana 3', 'Piñas', 'Jueves', 28],
+            ['Semana 3', 'Piñas', 'Viernes', 22],
+            ['Semana 3', 'Piñas', 'Sabado', 24],
+            ['Semana 3', 'Piñas', 'Domingo', 20],
+        ],
+        [
+            ['Semana 4', 'Piñas', 'Lunes', 35],
+            ['Semana 4', 'Piñas', 'Martes', 30],
+            ['Semana 4', 'Piñas', 'Miercoles', 33],
+            ['Semana 4', 'Piñas', 'Jueves', 32],
+            ['Semana 4', 'Piñas', 'Viernes', 30],
+            ['Semana 4', 'Piñas', 'Sabado', 29],
+            ['Semana 4', 'Piñas', 'Domingo', 28],
+        ],
+    ],
+    [
+        [
+            ['Semana 1', 'Portovelo', 'Lunes', 34],
+            ['Semana 1', 'Portovelo', 'Martes', 33],
+            ['Semana 1', 'Portovelo', 'Miercoles', 32],
+            ['Semana 1', 'Portovelo', 'Jueves', 31],
+            ['Semana 1', 'Portovelo', 'Viernes', 30],
+            ['Semana 1', 'Portovelo', 'Sabado', 32],
+            ['Semana 1', 'Portovelo', 'Domingo', 33]
+        ],
+        [
+            ['Semana 2', 'Portovelo', 'Lunes', 33],
+            ['Semana 2', 'Portovelo', 'Martes', 30],
+            ['Semana 2', 'Portovelo', 'Miercoles', 28],
+            ['Semana 2', 'Portovelo', 'Jueves', 29],
+            ['Semana 2', 'Portovelo', 'Viernes', 28],
+            ['Semana 2', 'Portovelo', 'Sabado', 27],
+            ['Semana 2', 'Portovelo', 'Domingo', 30],
+        ],
+        [
+            ['Semana 3', 'Portovelo', 'Lunes', 31],
+            ['Semana 3', 'Portovelo', 'Martes', 30],
+            ['Semana 3', 'Portovelo', 'Miercoles', 33],
+            ['Semana 3', 'Portovelo', 'Jueves', 28],
+            ['Semana 3', 'Portovelo', 'Viernes', 26],
+            ['Semana 3', 'Portovelo', 'Sabado', 25],
+            ['Semana 3', 'Portovelo', 'Domingo', 27],
+        ],
+        [
+            ['Semana 4', 'Portovelo', 'Lunes', 35],
+            ['Semana 4', 'Portovelo', 'Martes', 32],
+            ['Semana 4', 'Portovelo', 'Miercoles', 33],
+            ['Semana 4', 'Portovelo', 'Jueves', 31],
+            ['Semana 4', 'Portovelo', 'Viernes', 30],
+            ['Semana 4', 'Portovelo', 'Sabado', 28],
+            ['Semana 4', 'Portovelo', 'Domingo', 27],
+        ],
+    ],
+    [
+        [
+            ['Semana 1', 'Zaruma', 'Lunes', 33],
+            ['Semana 1', 'Zaruma', 'Martes', 33],
+            ['Semana 1', 'Zaruma', 'Miercoles', 32],
+            ['Semana 1', 'Zaruma', 'Jueves', 29],
+            ['Semana 1', 'Zaruma', 'Viernes', 30],
+            ['Semana 1', 'Zaruma', 'Sabado', 27],
+            ['Semana 1', 'Zaruma', 'Domingo', 28],
+        ],
+        [
+            ['Semana 2', 'Zaruma', 'Lunes', 29],
+            ['Semana 2', 'Zaruma', 'Martes', 28],
+            ['Semana 2', 'Zaruma', 'Miercoles', 26],
+            ['Semana 2', 'Zaruma', 'Jueves', 27],
+            ['Semana 2', 'Zaruma', 'Viernes', 28],
+            ['Semana 2', 'Zaruma', 'Sabado', 25],
+            ['Semana 2', 'Zaruma', 'Domingo', 24],
+        ],
+        [
+            ['Semana 3', 'Zaruma', 'Lunes', 28],
+            ['Semana 3', 'Zaruma', 'Martes', 27],
+            ['Semana 3', 'Zaruma', 'Miercoles', 26],
+            ['Semana 3', 'Zaruma', 'Jueves', 26],
+            ['Semana 3', 'Zaruma', 'Viernes', 25],
+            ['Semana 3', 'Zaruma', 'Sabado', 24],
+            ['Semana 3', 'Zaruma', 'Domingo', 28],
+        ],
+        [
+            ['Semana 4', 'Zaruma', 'Lunes', 35],
+            ['Semana 4', 'Zaruma', 'Martes', 30],
+            ['Semana 4', 'Zaruma', 'Miercoles', 33],
+            ['Semana 4', 'Zaruma', 'Jueves', 31],
+            ['Semana 4', 'Zaruma', 'Viernes', 27],
+            ['Semana 4', 'Zaruma', 'Sabado', 26],
+            ['Semana 4', 'Zaruma', 'Domingo', 29],
+        ],
+    ]
+]
+
+
+
+for ciudad in range(len(matr_temp)):#Bucles for anidados para recorrer los elementos
+    suma_mes = []
+    for semana in range(len(matr_temp[ciudad])):
+        suma = 0
+        for dia in range(len(matr_temp[ciudad][semana])):
+            for i in matr_temp[ciudad][semana][dia]:
+                if i in range(0,100):
+                    suma += i
+                suma_prom = suma/7
+        if 'Piñas' in matr_temp[ciudad][semana][dia][1]:
+            suma_mes.append(suma_prom)
+            print(f'El promedio de la semena {semana+1} es: {suma_prom:.2f} para la ciudad de', 'Piñas.')
+            if len(suma_mes)== 4:
+                suma_mes_prom = sum(suma_mes)/4
+                print(f'El promedio de la temperatura en Piñas en este mes fue de {suma_mes_prom:.2f}')
+        if 'Portovelo' in matr_temp[ciudad][semana][dia][1]:
+            suma_mes.append(suma_prom)
+            print(f'El promedio de la semena {semana+1} es: {suma_prom:.2f} para la ciudad de', 'Portovelo')
+            if len(suma_mes)== 4:
+                suma_mes_prom = sum(suma_mes)/4
+                print(f'El promedio de la temperatura en Portovelo en este mes fue de {suma_mes_prom:.2f}')
+        if 'Zaruma' in matr_temp[ciudad][semana][dia][1]:
+            suma_mes.append(suma_prom)
+            print(f'El promedio de la semena {semana+1} es: {suma_prom:.2f} para la ciudad de', 'Zaruma')
+            if len(suma_mes)== 4:
+                suma_mes_prom = sum(suma_mes)/4
+                print(f'El promedio de la temperatura en Zaruma en este mes fue de {suma_mes_prom:.2f}')
+
+
